904803a70c9995badbef038bf91fa741fa808efd
camilaleutz <leutz1989@gmail.com>
2022-09-07 16:55:23 -0300

    Correção

--- script.js
+++ script.js
@@ -19,11 +19,12 @@ slider.oninput = function(){
     for(let i = 0, n = charset.length; i <sliderElement.value; ++i){
        pass += charset.charAt(Math.floor(Math.random() * n));
     } 
-    containerPassword.classList.remove("hide")
-   password.innerHTML = pass;
+      containerPassword.classList.remove("hide")
+      password.innerHTML = pass;
+      novaSenha = pass;
  }
 
  function copyPassword(){
     alert("Senha copiada com sucesso!")
-    navigator.clipboard.writeText(novaSenha);
+    navigator.clipboard.writeText(novaSenha)
  }
